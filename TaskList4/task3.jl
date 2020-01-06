283730e7174a342b7d3a9121579e891995713b6d
Marcel Jerzyk <244979@student.pwr.edu.pl>
2020-01-06 20:16:43 +0100

    Polynomial Interpolation (natural form fix)

--- TaskList4/task3.jl
+++ TaskList4/task3.jl
@@ -1,18 +1,19 @@
 # Marcel Jerzyk [29.11.2019 | 20:43-]
+# 17.12.2019 Fix, was unfinished
 # Task 3.
 
 function naturalna(x::Vector{Float64}, fx::Vector{Float64})
 
-    n = length(fx)
-    coefficient = Vector{Float64}(n)
-    coefficient = fx[n] 
+    n = length(fx)] 
+    coefficient = zeros(n)
+    coefficient[n] = fx[n]
 
     for i = n - 1 : -1: 1
-        
+
         temp = coefficient[i + 1] * x[i]
         coefficient[i] = fx[i] - temp
 
-        for k in range(i + 1, n - 1)
+        for k = i + 1 : n - 1
 
             temp = coefficient[k + 1] * x[i]
             coefficient[k] = coefficient[k] - temp
